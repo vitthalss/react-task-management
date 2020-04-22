6054490a55388488651a7d9647979c99363b03ce
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-22 20:58:15 +0530

    BoardConstants.js
    initial commit

---
+++ src/constants/BoardConstants.js
@@ -0,0 +1,14 @@
+const boardConstants = {
+  fetch: 'BOARD_FETCH',
+  fetchSuccess: 'BOARD_FETCH_SUCCESS',
+  fetchFailure: 'BOARD_FETCH_FAILURE',
+  mergeTask: 'MERGE_TASK',
+  addTask: 'ADD_TASk',
+  updateTask: 'UPDATE_TASK',
+  deleteTask: 'DELETE_TASK',
+  addMembers: 'ADD_MEMBERS',
+  clear: 'CLEAR_BOARD',
+  deleteCard: 'DELETE_CARD'
+};
+
+export default boardConstants;
